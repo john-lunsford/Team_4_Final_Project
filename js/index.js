b7c9a817be109e06552b3993b5102163ab0d40ad
Frey <nm2ffave.gmail>
2021-05-13 16:01:29 -0400

    task 4 update

--- js/index.js
+++ js/index.js
@@ -2,56 +2,64 @@
 //Implement a JavaScript function named validFormFieldInput(data)
 //Add an ID attribute to each form field and implement the code needed to retrieve the each form field value using the following method:
  
+const newTaskNameInput = document.querySelector('#taskName');
+const newTaskDescriptionInput = document.querySelector('#taskDescription');
+const newAssignedToInput = document.querySelector('#assignedTo');
+const newDueDateInput = document.querySelector('#dueDate');
 
+const nullCheck = (name, description, assigned, due) => {
+  if ((name === '') && (description === '') && (assigned === '') && (due === '')){
+      return false;
+  }
+  else{
+      return true;
+  }
+}
 
-const button = document.getElementById ("clickButton");
-button.addEventListener('click', formCheck);
+function formCheck(isValid) {
+  var x = document.getElementById("formAlert");
+  if (isValid === true) {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
 
 function validFormFieldInput(){
 
-const newTaskNameInput = document.querySelector('#taskName');
+
 const name = newTaskNameInput.value;
 console.log('name: ' + name);
+return name;
 
-const newTaskDescriptionInput = document.querySelector('#taskDescription');
 const description = newTaskDescriptionInput.value;
 console.log('description: ' + description);
 
-const newAssignedToInput = document.querySelector('#assignedTo');
+
 const assigned = newAssignedToInput.value;
 console.log('assigned to: ' + assigned);
 
-const newDueDateInput = document.querySelector('#dueDate');
+
 const due = newDueDateInput.value;
 console.log('due: ' + due);
 
-nullCheck = () => {
-    if ((name === '') && (description === '') && (assigned === '') && (due === '')){
-        return false;
-    }
-    else{
-        return true;
-    }
+const formValid = nullCheck(name, description, assigned, due);
+formCheck(formValid);
 }
 
-}
 
-function formCheck() {
-    var x = document.getElementById("formAlert");
-    if (nullCheck === false) {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
-  }
+const button = document.getElementById ("clickButton");
+button.addEventListener('click', validFormFieldInput);
 
 
-
-validFormFieldInput();
+// validFormFieldInput();
 
 
 let newTask = new TaskManager();
-newTask.addTask();
+newTask.addTask(validFormFieldInput);
 console.log(newTask.tasks);
+
+// button .addeventListener ('Click', newTask);
+// mysteryButton .addeventListener('wheel', newTask);
 
 //Log your field inputs to verify that you are getting the data you need to validate.
