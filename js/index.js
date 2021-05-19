2c2cce8915a7b95c7157812c96b6408d500ccea6
john-lunsford <81324379+john-lunsford@users.noreply.github.com>
2021-05-19 16:35:00 -0400

    almost done

--- js/index.js
+++ js/index.js
@@ -1,9 +1,11 @@
 //Create a JavaScript file named index.js and include it into your index.html page.
 //Implement a JavaScript function named validFormFieldInput(data)
 //Add an ID attribute to each form field and implement the code needed to retrieve the each form field value using the following method:
- 
 
+// In js/index.js, near the top of the file, after instantiating taskManager, load the tasks with 
+// taskManager.load() and render them with taskManager.render().
 
+let newTask = new TaskManager();
 
 const newTaskNameInput = document.querySelector('#taskName');
 const newTaskDescriptionInput = document.querySelector('#taskDescription');
@@ -11,15 +13,15 @@ const newAssignedToInput = document.querySelector('#assignedTo');
 const newDueDateInput = document.querySelector('#dueDate');
 
 const nullCheck = (name, description, assigned, due) => {
-  if ((name === '') || (description === '') || (assigned === '') || (due === '')){
-      return false;
+  if ((name === '') || (description === '') || (assigned === '') || (due === '')) {
+    return false;
   }
-  else{
-      return true;
+  else {
+    return true;
   }
 }
 
-let newTask = new TaskManager();
+
 
 function formCheck(isValid) {
   var x = document.getElementById("formAlert");
@@ -30,58 +32,102 @@ function formCheck(isValid) {
   }
 }
 
-function validFormFieldInput(){
+function validFormFieldInput() {
 
 
-const name = newTaskNameInput.value;
-//console.log('name: ' + name);
+  const name = newTaskNameInput.value;
+  //console.log('name: ' + name);
 
-const description = newTaskDescriptionInput.value;
-//console.log('description: ' + description);
+  const description = newTaskDescriptionInput.value;
+  //console.log('description: ' + description);
 
-const assigned = newAssignedToInput.value;
-//console.log('assigned to: ' + assigned);
+  const assigned = newAssignedToInput.value;
+  //console.log('assigned to: ' + assigned);
 
-const due = newDueDateInput.value;
-//console.log('due: ' + due);
+  const due = newDueDateInput.value;
+  //console.log('due: ' + due);
 
-const formValid = nullCheck(name, description, assigned, due);
-formCheck(formValid);
-if (formValid){
-  newTask.addTask(name, description, assigned, due);
-  newTaskNameInput.value='';
-  newTaskDescriptionInput.value='';
-  newAssignedToInput.value='';
-  newDueDateInput.value='';
+  const formValid = nullCheck(name, description, assigned, due);
+  formCheck(formValid);
+  if (formValid) {
+    newTask.addTask(name, description, assigned, due);
+    newTaskNameInput.value = '';
+    newTaskDescriptionInput.value = '';
+    newAssignedToInput.value = '';
+    newDueDateInput.value = '';
+    newTask.save();
 
+  }
+  console.log(newTask.tasks);
+  const taskCardsHtml = newTask.render();
+  listGroup.innerHTML = taskCardsHtml;
 }
-console.log(newTask.tasks);
-const taskCardsHtml = newTask.render();
-listGroup.innerHTML = taskCardsHtml;
-}
 
 
-const button = document.getElementById ("clickButton");
+const button = document.getElementById("clickButton");
 button.addEventListener('click', validFormFieldInput);
 
 
-// const doneButton = document.querySelector('#doneButton');
+
+
+// Now we have our deleteTask method ready, we need to connect it to the delete buttons we created 
+// in Step 1.
+
+// We'll be using the delete-button class we added to the buttons to find them. It's all very similar
+//  to the code we did for the "Mark As Done" button. After the deleting the task, remember to 
+// taskManager.save() and taskManager.render() the tasks!
+
+//     1. In js/index.js, find the EventListener for the click event on the Tasks List we created in Task 8.
+//     2. At the bottom of the function, after our code that handles the "Mark As Done" button, 
+// create a new if statement to check if the event.target.classList contains the class 'delete-button'.
+//     3. If it does, get the parentTask and store it as a variable.
+//     4. Get the taskId of the parent task from its data-task-id property - remember, since it's stored 
+// as a string in a data attribute, we need to convert it to a number, just like we did for task 8!
+//     5. Delete the task, passing the taskId to taskManager.deleteTask()
+//     6. Save the tasks to localStorage using taskManager.save()
+//     7. Render the tasks using taskManager.render().
+
+
 
 const listGroup = document.getElementById('listGroup');
 
-listGroup.addEventListener('click', (event) => { 
-    if (event.target.classList.contains ("doneButton")){
-      const parentTask = event.target.parentElement.parentElement.parentElement;
-      // return parentTask;
-      console.log(parentTask);
-    }
+listGroup.addEventListener('click', (event) => {
+  if (event.target.classList.contains("doneButton")) {
+    const parentTask = event.target.parentElement.parentElement.parentElement;
+    // return parentTask;
+    const taskId = Number(parentTask.dataset.taskId);
+    // console.log(taskId);
+    const task = newTask.getTaskById(taskId);
+    task.status = 'DONE'
+    const taskCardsHtml = newTask.render();
+    listGroup.innerHTML = taskCardsHtml;
+    newTask.save();
+ if (event.target.classList.contains("deleteButton")) {
+    const parentTask = event.target.parentElement.parentElement.parentElement;
+    const taskId = Number(parentTask.dataset.taskId);
+    const task = newTask.getTaskById(taskId);
+    newTask.deleteTasks(task); ///this is the line that I do not know if it is corret
+    newTask.save();
+    const taskCardsHtml = newTask.render();
+    listGroup.innerHTML = taskCardsHtml;
+    
+ }
+
+    // console.log (task.status);
+  }
+
+
+
+
   // console.log(parentTask);
 });
 // const taskHtml = createTaskHtml('AA', 'dooo', 'Mary', 23/05/2022);
 // console.log(taskHtml);
 
 
-
+newTask.load();
+const taskCardsHtml = newTask.render();
+listGroup.innerHTML = taskCardsHtml;
 
 // validFormFieldInput();
 
--- js/taskManager.js
+++ js/taskManager.js
@@ -1,7 +1,7 @@
 function createTaskHtml(name, description, assigned, due, status, id) {
-    const html = `<div class="list-group-item">
+    const html = `<div class="list-group-item" data-task-id = "${id}">
                       <div class="card border-dark mb-3" style="max-width: 28rem;">
-                           <div class="card-header">${id}</div>
+                           <div class="card-header">Task ${id}</div>
                            <div class="card-body text-dark">
                                 <h5 class="card-title">${name}</h5>
                                 <h6 class="card-text">${description}.</h6>
@@ -10,7 +10,10 @@ function createTaskHtml(name, description, assigned, due, status, id) {
                                 <h6 class="card-text">${status}</h6>
                            </div>
                            <div class="d-grid gap-2 d-md-flex justify-content-md-end">
-                                  <button type="button" class="btn btn-primary btn-sm doneButton">Done</button>
+                                  <button type="button" class="btn btn-danger btn-sm deleteButton">Delete</button>
+                           </div>
+                           <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+                                  <button type="button" class="btn btn-success btn-sm doneButton">Done</button>
                            </div>
                      </div>
                  </div>`;
@@ -41,33 +44,86 @@ class TaskManager {
         console.log(tasksHtml);
         return tasksHtml;
 
-        
-
     }
-    
-
-// In the getTaskById() method, create a foundTask variable to store the found task.
-// Note Since we'll be mutating (modifying) this variable, be sure to declare it with let!
-// Loop over the this.tasks array, for each task in the loop:
-//     Store the current task in a variable called task 
-//     Compare task.id to the passed in taskId, if its a match, store the task to the variable foundTask
-// After the loop, return the foundTask variable from the method.
-
 
     getTaskById(taskId){
-        let foundTask = [];
+        let foundTask
         for (let i = 0; i < this.tasks.length; i++){
         const task = this.tasks[i];
         if (task.id === taskId) {
-        return task;
+        foundTask = task;
         }
         }
         return foundTask;
     }
+
+
+    // In js/taskManager.js, in the TaskManager class, create a save method. This method doesn't require 
+    // any parameters.
+    // In the save method, create a JSON string of the tasks using JSON.stringify() and store it to a 
+    // new variable, tasksJson.
+    // Store the JSON string in localStorage under the key tasks using localStorage.setItem().
+    // Convert the this.currentId to a string and store it in a new variable, currentId.
+    // Store the currentId variable in localStorage under the key currentId using localStorage.setItem().
+    // In js/index.js, after both adding a new task and updating a task's status to done, call 
+    // taskManager.save() to save the tasks to localSorage.
+
+    save(){
+       const tasksJson = JSON.stringify(this.tasks);
+       localStorage.setItem('tasks', tasksJson);
+       const currentId = String (this.currentId);
+       localStorage.setItem('currentId', currentId);
+
+    }
+
+    // In js/taskManager.js, add a new method called load. This method doesn't require any parameters.
+    // In the load method, check if any tasks are saved in localStorage with localStorage.getItem().
+    // If any tasks are stored, get the JSON string of tasks stored in localStorage with 
+    // localStorage.getItem(), making sure to pass the key we used to save the tasks, tasks. 
+    // Store this string into a new variable, tasksJson.
+    // Convert the tasksJson string to an array using JSON.parse() and store it in this.tasks.
+    // Next, check if the currentId is saved in localStorage with localStorage.getItem().
+    // If the currentId is stored, get the currentId in localStorage using localStorage.getItem() 
+    // and store it in a new variable, currentId.
+    // Convert the currentId to a number before storing it to the TaskManager's this.currentId
+    // In js/index.js, near the top of the file, after instantiating taskManager, load the tasks with 
+    // taskManager.load() and render them with taskManager.render().
+   
+    load (){
+        const tasksJson = localStorage.getItem('tasks');
+        if (tasksJson){
+        this.tasks = JSON.parse(tasksJson);
+        console.log(this.tasks);
+        }
+        let currentId = localStorage.getItem('currentId');
+        this._currentId = Number (currentId);
+        console.log(currentId);
+    // }
+    }
+
+
+
+    // In js/taskManager.js, create a deleteTask method on the TaskManager class. It should take one 
+    // parameter, taskId, the id of the task we want deleted.
+    // In the deleteTask method, create an empty array and store it in a new variable, newTasks.
+    // Loop over the tasks, in the loop
+    //     Get the current task in the loop, store it in a variable, task.
+    //     Check if task.id is not equal to the taskId passed as a parameter.
+    //     If the task.id is not equal to the taskId, push the task into the newTasks array.
+    // Set this.tasks to newTasks.
+
+    deleteTasks(taskId) {
+        newTasks = [];
+        for (let i = 0; i < this.tasks.length; i++) {
+            const task = this.tasks[i]
+            if (task.id !== taskId) {
+                newTasks.push(task);
+            }
+            this.tasks = newTask;
+        }
+    }
+
+
+
+
 }
-
-
-    
-    
-    
-
